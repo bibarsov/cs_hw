bb3df475d4caff6a86b8b1418185b0102e77fafa
Bibarsov <tbibarsov@wiley.com>
2023-03-13 16:58:47 +0300

    hw4

---
+++ sem4/task25/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+    3, 5 -> 243 (3⁵)
+    2, 4 -> 16
+ */
+namespace task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Pow(a, b));
+        }
+
+        static int Pow(int n, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= n;
+            }
+            return result;
+        }
+    }
+}
---
+++ sem4/task29/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+    Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+    1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+    6, 1, 33-> [6, 1, 33]
+ */
+namespace task29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = ReadInteger("Enter array length:");
+            int min = ReadInteger("Enter range minimum for a value in array:");
+            int max = ReadInteger("Enter range maximum for a value in array:");
+
+            Console.WriteLine(string.Join(",", GenerateArray(len, min, max + 1)));
+        }
+
+        private static int[] GenerateArray(int len, int min, int maxExclusive)
+        {
+            int[] result = new int[len];
+            Random randomizer = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                result[i] = randomizer.Next(min, maxExclusive);
+            }
+            return result;
+        }
+
+        static int ReadInteger(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
