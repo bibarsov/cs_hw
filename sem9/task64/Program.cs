f60f3182872ec3b19adf97b0d301283f9d743968
Bibarsov <tbibarsov@wiley.com>
2023-03-30 19:57:10 +0300

    hw9

---
+++ sem9/task64/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
+ */
+namespace task64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = ReadInteger("Enter N:");
+            if (n < 1)
+            {
+                Console.WriteLine("Only N>=1 should be provided.");
+                return;
+            }
+            PrintNaturalNumsUpTo(n);
+            Console.WriteLine();
+        }
+
+        static int PrintNaturalNumsUpTo(int n)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(n);
+                return 0;
+            }
+            Console.Write("{0}, ", n);
+
+            return PrintNaturalNumsUpTo(n - 1);
+
+        }
+
+        static int ReadInteger(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ sem9/task68/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+ */
+namespace task68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Warning: error prone solution (Stack overflow).");
+            Console.WriteLine("Please use arguments those are easy to compute within this function.");
+            int m = ReadInteger("Enter M:");
+            int n = ReadInteger("Enter N:");
+
+            if (m < 0 || n < 0)
+            {
+                Console.WriteLine("M or/and N should be non-negative numbers");
+                return;
+            }
+
+            long result = Ackermann(m, n);
+            Console.WriteLine(result);
+        }
+
+        static long Ackermann(long m, long n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            if (m > 0 && n == 0)
+            {
+                return Ackermann(m - 1, 1);
+            }
+
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+
+        static int ReadInteger(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
